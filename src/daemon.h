d343e654f502a8209f52e1cf411fc7ef30beec46
Gilberto "Xyhthyx" Miralla <xyhthyx@gmail.com>
2008-08-28 00:55:11 +0000

    Fix daemon mode
    git-svn-id: svn://svn.code.sf.net/p/parcellite/code/trunk@27 b25b1718-469e-46af-be73-919d414b4ebe

---
+++ src/daemon.h
@@ -0,0 +1,33 @@
+/* Copyright (C) 2007-2008 by Xyhthyx <xyhthyx@gmail.com>
+ *
+ * This file is part of Parcellite.
+ *
+ * Parcellite is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Parcellite is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DAEMON_H
+#define DAEMON_H
+
+G_BEGIN_DECLS
+
+/* Defines */
+#define PRIMCHECKDELAY 3000
+
+/* Functions */
+void
+init_daemon_mode();
+
+G_END_DECLS
+
+#endif
