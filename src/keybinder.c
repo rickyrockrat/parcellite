dbe886baa349b1e49dca0100607494411a90b4bc
Gilberto Miralla <xyhthyx@gmail.com>
2009-12-26 14:51:28 +0000

    * Merged Ludwig Nussel's patch to silence gcc warnings. * Updated changelog.
    git-svn-id: http://svn.code.sf.net/p/parcellite/code/trunk@105 b25b1718-469e-46af-be73-919d414b4ebe

--- src/keybinder.c
+++ src/keybinder.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-
+#include <string.h>
 #include <gdk/gdk.h>
 #include <gdk/gdkwindow.h>
 #include <gdk/gdkx.h>
