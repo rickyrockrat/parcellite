dee58fddd7b2b0098fee11b4adc552d4c42c7dc8
RickyRockRat <gpib@rickyrockrat.com>
2011-03-19 04:37:06 +0000

    Fixed clear not working with type-as-you-search off
    git-svn-id: http://svn.code.sf.net/p/parcellite/code/trunk@183 b25b1718-469e-46af-be73-919d414b4ebe

--- src/main.c
+++ src/main.c
@@ -1010,9 +1010,10 @@ static gboolean show_history_menu(gpointer data)
   gtk_menu_shell_append((GtkMenuShell*)menu, gtk_separator_menu_item_new());
   /* Clear */
 	if(0 ==prefs.type_search){
-  menu_item = gtk_image_menu_item_new_with_label(_("Clear"));
+  menu_item = gtk_image_menu_item_new_with_mnemonic(_("_Clear"));
   menu_image = gtk_image_new_from_stock(GTK_STOCK_CLEAR, GTK_ICON_SIZE_MENU);
   gtk_image_menu_item_set_image((GtkImageMenuItem*)menu_item, menu_image);
+	g_signal_connect((GObject*)menu_item, "activate", (GCallback)clear_selected, NULL);
   gtk_menu_shell_append((GtkMenuShell*)menu, menu_item);
   }  
   /* Popup the menu... */
