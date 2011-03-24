952e435595ac79c59f036fd3eeab3e7197f3fa9b
RickyRockRat <gpib@rickyrockrat.net>
2011-03-24 21:25:50 +0000

    Added debian package files and build script
    git-svn-id: svn://svn.code.sf.net/p/parcellite/code/trunk@192 b25b1718-469e-46af-be73-919d414b4ebe

---
+++ build-debpkg.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+if [ $# -lt 1 ]; then
+	echo "Usage $0 /path/to/source/tar"
+	exit 1
+fi
+DEBEMAIL=gpib@rickyrockrat.net
+DEBFULLNAME="Doug Springer"
+export DEBFULLNAME DEBEMAIL
+_ARCH=$(uname -m);
+case $_ARCH in
+  x86_64) ARCH="amd64";;
+  i686|i586|i486|i386) ARCH="i386";;
+  *) echo "Unknown machine '$_ARCH'. Add machine to this build"
+  exit 1;;
+esac
+dh_make -s -f $1
+cd debian
+rm emacsen-* manpage.* init.d.ex README.Debian 
+cd ..
+cp deb/copyright debian
+sed "s/ADD_ARCH_HERE/$ARCH/g" deb/control >debian/control
+dpkg-buildpackage -rfakeroot
+
