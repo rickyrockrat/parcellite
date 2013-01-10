1597910dfef5f5908bc01b7b8c4470b28c7fe7bb
RickyRockRat <gpib@rickyrockrat.com>
2013-01-10 06:58:42 +0000

    Moved structs from main.c, added histno, chage_flag
    git-svn-id: http://svn.code.sf.net/p/parcellite/code/trunk@279 b25b1718-469e-46af-be73-919d414b4ebe

--- src/main.h
+++ src/main.h
@@ -27,7 +27,38 @@ G_BEGIN_DECLS
 #define ID_PRIMARY   0
 #define ID_CLIPBOARD 1
 
+#define HIST_DISPLAY_NORMAL     0
+#define HIST_DISPLAY_PERSISTENT	1
+
+#define KBUF_SIZE 20
+struct widget_info{
+	GtkWidget *menu; /**top level history list window  */
+	GtkWidget *item; /**item we are looking at  */
+	 GdkEventKey *event; /**event info where we filled this struct  */
+	gint index;      /**index into the array  */
+	gint tmp1;			 /**gp vars  */
+	gint tmp2;
+};
+/**keeps track of each menu item and the element it's created from.  */
+struct s_item_info {
+	GtkWidget *item;
+	GSList *element;
+};
+
+struct history_info{
+  GtkWidget *menu;			/**top level history menu  */
+  GtkWidget *clip_item; /**currently selected history item (represents clipboard)  */
+	gchar *element_text;	/**texts of selected history clipboard item  */
+  GtkWidget *title_item;
+	GList *delete_list; /**struct s_item_info - for the delete list  */
+	GList *persist_list; /**struct s_item_info - for the persistent list  */
+	struct widget_info wi;  /**temp  for usage in popups  */
+	gint histno;           /**which history?  HIST_DISPLAY_NORMAL/HIST_DISPLAY_PERSISTENT*/
+	gint change_flag;	/**bit wise flags for history state  */
+};
+
 int p_strcmp (const char *str1, const char *str2);
+void phistory_hotkey(char *keystring, gpointer user_data);
 void history_hotkey(char *keystring, gpointer user_data);
 void actions_hotkey(char *keystring, gpointer user_data);
 void menu_hotkey(char *keystring, gpointer user_data);
