6dc71d58d5d1a5ca9f5c48c89c3056de5bb823d0
RickyRockRat <gpib@rickyrockrat.net>
2017-01-24 02:46:14 -0700

    Fix ppa script so it gives us a clue.

--- rel.ppa.sh
+++ rel.ppa.sh
@@ -16,7 +16,30 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 . ./rel.common
+appind_dput='[parcellite-appindicator]
+fqdn                    = ppa.launchpad.net
+method                  = ftp
+incoming                = ~rickyrockrat/ubuntu/parcellite-appindicator/
+login                   = anonymous
+'
+noind_dput='[parcellite]
+fqdn                    = ppa.launchpad.net
+method                  = ftp
+incoming                = ~rickyrockrat/ubuntu/parcellite/
+login                   = anonymous
+'
 set_abs_path
+is_dput_app=$(grep -c 'parcellite-appindicator' /etc/dput.cf)
+is_dput_noapp=$(grep -c 'parcellite' /etc/dput.cf)
+if [ "0" = "$is_dput_app" ] || [ "0" = "$is_dput_noapp" ]; then
+	echo "/etc/dput.cf is not set up." 
+	echo "May also need devscripts installed"
+	echo "put the following entries into dput.cf:"
+	echo "$appind_dput"
+	echo ""
+	echo "$noind_dput"
+	exit 1
+fi
 
 cd "$CDIR"
 set_scm_local
