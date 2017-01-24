a7931d8cf983d6c89d94e39746bc8b21183f90c4
RickyRockRat <gpib@rickyrockrat.net>
2017-01-23 20:17:38 -0700

    scm agnostic changes

--- svnversion.sh
+++ svnversion.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
- 
-echo "$0">svnversion.log
+LOG="scmversion.log" 
+TMP=_scmversion_test
+echo "$0">$LOG
 if [ -n "$1" ]; then
-  echo "Immediate Mode">>svnversion.log
+  echo "Immediate Mode">>$LOG
   IM=1
 else
   IM=0
 fi
-which svnversion > _svnversion_test
+which svnversion > $TMP
+which git >> $TMP
 if [ $? -ne 0 ] ;then
   echo "svn"
 else
@@ -28,13 +30,18 @@ else
    FULLPATH="$CWD"
   fi
   cd "$FULLPATH"
-  echo "Using $FULLPATH">>"$CWD/svnversion.log"
-  SVER=$(svn info .|grep -i "revision"|sed 's!.*: !!'|tr -d ' ')
-  echo -n "svn$SVER"
+  echo "Using $FULLPATH">>"$CWD/$LOG"
+  if [ -e .svn ]; then
+    SVER=$(svn info .|grep -i "revision"|sed 's!.*: !!'|tr -d ' ')
+    SVER="svn-$SVER"
+  elif [ -e .git ]; then
+    SVER="git-$(git log -n1 --pretty=format:%h)"
+  fi
+  echo -n "$SVER"
   cd $CWD
   if [ "$IM" = "1" ]; then
-    sed -i "s#\(.*VERSION \).*#\1\"svn$SVER\"#" config.h
-    echo "Make sure to do svn up in source svn to update local svn info, then run again."
+    sed -i "s#\(.*VERSION \).*#\1\"$SVER\"#" config.h
+    echo "Make sure to do scm update in source scm to update local scm info, then run again."
     rm src/main.o
     echo "Removed src/main.o. Recompile."
   fi
